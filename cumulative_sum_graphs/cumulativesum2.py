2bfbaa948961b5fa376930a0aa6b1c8ec77ecf9a
gasimmons <146766169+gasimmons@users.noreply.github.com>
2024-07-17 16:49:58 -0700

    Create cumulativesum2.py

---
+++ cumulative_sum_graphs/cumulativesum2.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import sys
+import os
+import numpy as np
+
+def read_data(file_path):
+    ids = []
+    lengths = []
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                id_str, length_str = line.strip().split()
+                ids.append(id_str)
+                lengths.append(int(length_str))
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        sys.exit(1)
+    return ids, lengths
+
+def plot_cumulative_sum(files, output_file):
+    plt.figure(figsize=(20, 12))
+    plt.xlabel('Number of Fragments (log)')
+    plt.ylabel('Assembly Size (M)')
+    plt.title('Avocado Assembly Cumulative Sum Graph')
+    plt.tight_layout()
+
+    for file_path in files:
+        ids, lengths = read_data(file_path)
+        cumulative_sum = [sum(lengths[:i+1]) for i in range(len(lengths))]
+        num_fragments = np.arange(1, len(ids) + 1)
+        plt.plot(num_fragments, [x / 1000000 for x in cumulative_sum], label=os.path.basename(file_path))
+
+    plt.xscale('log')
+    plt.grid(True)
+    plt.legend()
+
+    plt.axvline(x=12, linestyle='--', color='black')
+  
+    plt.savefig(output_file, dpi=600, bbox_inches='tight')
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python script.py <file_path1> <file_path2> ... <file_pathN>")
+        sys.exit(1)
+
+    files = sys.argv[1:]
+
+    current_dir = os.getcwd()
+    output_file = os.path.join(current_dir, "cumulative_sum_graph2.pdf")
+
+    plot_cumulative_sum(files, output_file)
