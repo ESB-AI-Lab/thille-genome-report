2dd8915da7aee46f72bb32e1451f3d19f63f6451
gasimmons <146766169+gasimmons@users.noreply.github.com>
2024-08-07 21:17:48 -0700

    Create 1_sbatch_map.sh

---
+++ top1pct_FST_onto_thille/1_sbatch_map.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J map		# jobname
+#SBATCH -o map.o%A.%a	# jobname.o%j for single (non array) jobs jobname.o%A.%a for array jobs
+#SBATCH -e map.e%A.%a	# error file name A is the jobid and a is the arraytaskid
+#SBATCH -a 1		# start and stop of the array start-end
+#SBATCH -A mcb180013p
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH -p RM-shared		# queue (partition) -- compute, shared, large-shared, debug (30mins)
+#SBATCH -t 48:00:00		# run time (dd:hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=kac012@ucsd.edu
+###SBATCH --mail-type=begin	# email me when the job starts
+###SBATCH --mail-type=end	# email me when the job finishes
+#SBATCH --export=ALL
+
+PATH=/jet/home/kchene/bin/minimap2-2.24_x64-linux:$PATH
+
+gwen_fasta=gwen_braker_rnaseq_hapsolo_annotation.genes.avo_typeA_v_typeB.top1pct.20k.windowed.weir.stranded.fasta
+thille_fasta=thille.hifiv3.asm.p_ctg.softmasked.fasta
+
+minimap2 -ax splice $thille_fasta $gwen_fasta  > top1pct_onto_thille.sam
